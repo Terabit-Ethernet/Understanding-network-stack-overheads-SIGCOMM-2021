c0fca826103accfd042209e4d17682a96cddd972
qizhe cai <qizhe.cai@gmail.com>
2021-04-06 14:37:43 -0400

    Update all-to-all.sh

--- receiver/all-to-all.sh
+++ receiver/all-to-all.sh
@@ -1,23 +1,24 @@
+iface=$1
 # No Optimisations
-./network_setup.py enp37s0f1 --no-lro --no-gso --no-gro --no-tso --no-arfs --config all-to-all --receiver --mtu 1500 --sock-size
+./network_setup.py $iface --no-lro --no-gso --no-gro --no-tso --no-arfs --config all-to-all --receiver --mtu 1500 --sock-size
 for i in 2 4 6 8 12; do
         ./run_experiment_receiver.py --config all-to-all --num-connections $i --throughput --utilisation --cache-miss --output results/all-to-all_${i}_no-opts | tee results/all-to-all_${i}_no-opts.log
 done
 
 # TSO/GRO
-./network_setup.py enp37s0f1 --gro --tso 
+./network_setup.py $iface --gro --tso 
 for i in 2 4 6 8 12; do
         ./run_experiment_receiver.py --config all-to-all --num-connections $i --throughput --utilisation --cache-miss --output results/all-to-all_${i}_tsogro | tee results/all-to-all_${i}_tsogro.log
 done
 
 # TSO/GRO+Jumbo Frame
-./network_setup.py enp37s0f1 --mtu 9000
+./network_setup.py $iface --mtu 9000
 for i in 2 4 6 8 12; do
         ./run_experiment_receiver.py --config all-to-all --num-connections $i --throughput --utilisation --cache-miss --output results/all-to-all_${i}_tsogro+jumbo | tee results/all-to-all_${i}_tsogro+jumbo.log
 done
 
 # TSO/GRO+Jumbo Frame+aRFS
-./network_setup.py enp37s0f1 --arfs
+./network_setup.py $iface --arfs
 for i in 2 4 6 8 12; do
         ./run_experiment_receiver.py --config all-to-all --num-connections $i --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/all-to-all_${i}_all-opts | tee results/all-to-all_${i}_all-opts.log
 done
