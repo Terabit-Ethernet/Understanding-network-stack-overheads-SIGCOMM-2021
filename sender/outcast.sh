f099134db68f2ba623b3afaf21d23f8fdf63fa57
qizhe cai <qizhe.cai@gmail.com>
2021-04-06 14:32:18 -0400

    Update outcast.sh

--- sender/outcast.sh
+++ sender/outcast.sh
@@ -1,23 +1,27 @@
+public_dst_ip=$1
+device_dst_ip=$2
+iface=$3
+# Example: ./outcast.sh 128.84.155.115 192.168.10.115 enp1
 # No Optimisations
-./network_setup.py enp37s0f1 --no-lro --no-gso --no-gro --no-tso --no-arfs --sender --config outcast --mtu 1500 --sock-size
+./network_setup.py $iface --no-lro --no-gso --no-gro --no-tso --no-arfs --sender --config outcast --mtu 1500 --sock-size
 for i in 2 4 8; do
-        ./run_experiment_sender.py --addr 192.168.10.115 --receiver genie02 --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_no-opts | tee results/outcast_${i}_no-opts.log
+        ./run_experiment_sender.py --addr $device_dst_ip --receiver $public_dst_ip --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_no-opts | tee results/outcast_${i}_no-opts.log
 done
 
 # TSO/GRO
-./network_setup.py enp37s0f1 --gro --tso
+./network_setup.py $iface --gro --tso
 for i in 2 4 8; do
-        ./run_experiment_sender.py --addr 192.168.10.115 --receiver genie02 --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro | tee results/outcast_${i}_tsogro.log
+        ./run_experiment_sender.py --addr $device_dst_ip --receiver $public_dst_ip --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro | tee results/outcast_${i}_tsogro.log
 done
 
 # TSO/GRO+Jumbo Frame
-./network_setup.py enp37s0f1 --mtu 9000
+./network_setup.py $iface --mtu 9000
 for i in 2 4 8; do
-        ./run_experiment_sender.py --addr 192.168.10.115 --receiver genie02 --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro+jumbo | tee results/outcast_${i}_tsogro+jumbo.log
+        ./run_experiment_sender.py --addr $device_dst_ip --receiver $public_dst_ip --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro+jumbo | tee results/outcast_${i}_tsogro+jumbo.log
 done
 
 # TSO/GRO+Jumbo Frame+aRFS
-./network_setup.py enp37s0f1 --arfs
+./network_setup.py $iface --arfs
 for i in 2 4 8; do
-        ./run_experiment_sender.py --addr 192.168.10.115 --receiver genie02 --config outcast --num-connections $i --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/outcast_${i}_all-opts | tee results/outcast_${i}_all-opts.log
+        ./run_experiment_sender.py --addr $device_dst_ip --receiver $public_dst_ip --config outcast --num-connections $i --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/outcast_${i}_all-opts | tee results/outcast_${i}_all-opts.log
 done
