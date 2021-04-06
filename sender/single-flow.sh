39335e3e2d18670200e0b9cbe37c40f88ac0536b
qizhe cai <qizhe.cai@gmail.com>
2021-04-06 14:34:47 -0400

    Update single-flow.sh

--- sender/single-flow.sh
+++ sender/single-flow.sh
@@ -1,27 +1,31 @@
+public_dst_ip=$1
+device_dst_ip=$2
+iface=$3
+# Example: ./incast.sh 128.84.155.115 192.168.10.115 enp37s0f1
 # No Optimisations
-./network_setup.py enp37s0f1 --no-lro --no-gso --no-gro --no-tso --no-arfs --sender --mtu 1500 --sock-size
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_no-opts | tee results/single-flow_no-opts.log
+./network_setup.py $iface --no-lro --no-gso --no-gro --no-tso --no-arfs --sender --mtu 1500 --sock-size
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_no-opts | tee results/single-flow_no-opts.log
 
 # TSO/GRO
-./network_setup.py enp37s0f1 --gro --tso
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_tsogro | tee results/single-flow_tsogro.log
+./network_setup.py $iface --gro --tso
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_tsogro | tee results/single-flow_tsogro.log
 
 # Jumbo
-./network_setup.py enp37s0f1 --no-gro --no-tso --mtu 9000
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_jumbo | tee results/single-flow_jumbo.log
+./network_setup.py $iface --no-gro --no-tso --mtu 9000
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_jumbo | tee results/single-flow_jumbo.log
 
 # TSO/GRO+Jumbo Frame
-./network_setup.py enp37s0f1 --gro --tso
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_tsogro+jumbo | tee results/single-flow_tsogro+jumbo.log
+./network_setup.py $iface --gro --tso
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --output results/single-flow_tsogro+jumbo | tee results/single-flow_tsogro+jumbo.log
 
 # TSO/GRO+aRFS
-./network_setup.py enp37s0f1 --gro --tso --arfs --mtu 1500
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_tsogro+arfs | tee results/single-flow_tsogro+arfs.log
+./network_setup.py $iface --gro --tso --arfs --mtu 1500
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_tsogro+arfs | tee results/single-flow_tsogro+arfs.log
 
 # Jumbo+aRFS
-./network_setup.py enp37s0f1 --no-gro --no-tso --mtu 9000
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_jumbo+arfs | tee results/single-flow_jumbo+arfs.log
+./network_setup.py $iface --no-gro --no-tso --mtu 9000
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_jumbo+arfs | tee results/single-flow_jumbo+arfs.log
 
 # TSO/GRO+Jumbo Frame+aRFS
-./network_setup.py enp37s0f1 --gro --tso
-./run_experiment_sender.py --receiver genie02 --addr 192.168.10.115 --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_all-opts | tee results/single-flow_all-opts.log
+./network_setup.py $iface --gro --tso
+./run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/single-flow_all-opts | tee results/single-flow_all-opts.log
