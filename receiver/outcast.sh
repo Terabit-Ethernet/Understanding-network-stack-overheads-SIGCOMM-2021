1da6be2814bd7a8c4e261b1621ee1578360971d9
qizhe cai <qizhe.cai@gmail.com>
2021-04-06 14:39:22 -0400

    Update outcast.sh

--- receiver/outcast.sh
+++ receiver/outcast.sh
@@ -1,23 +1,24 @@
+iface=$1
 # No Optimisations
-./network_setup.py enp37s0f1 --no-lro --no-gso --no-gro --no-tso --no-arfs --config outcast --receiver --mtu 1500 --sock-size
+./network_setup.py $iface --no-lro --no-gso --no-gro --no-tso --no-arfs --config outcast --receiver --mtu 1500 --sock-size
 for i in 2 4 8; do
         ./run_experiment_receiver.py --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_no-opts | tee results/outcast_${i}_no-opts.log
 done
 
 # TSO/GRO
-./network_setup.py enp37s0f1 --gro --tso 
+./network_setup.py $iface --gro --tso 
 for i in 2 4 8; do
         ./run_experiment_receiver.py --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro | tee results/outcast_${i}_tsogro.log
 done
 
 # TSO/GRO+Jumbo Frame
-./network_setup.py enp37s0f1 --mtu 9000
+./network_setup.py $iface --mtu 9000
 for i in 2 4 8; do
         ./run_experiment_receiver.py --config outcast --num-connections $i --throughput --utilisation --cache-miss --output results/outcast_${i}_tsogro+jumbo | tee results/outcast_${i}_tsogro+jumbo.log
 done
 
 # TSO/GRO+Jumbo Frame+aRFS
-./network_setup.py enp37s0f1 --arfs
+./network_setup.py $iface --arfs
 for i in 2 4 8; do
         ./run_experiment_receiver.py --config outcast --num-connections $i --throughput --utilisation --cache-miss --util-breakdown --arfs --output results/outcast_${i}_all-opts | tee results/outcast_${i}_all-opts.log
 done
