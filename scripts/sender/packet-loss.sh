90159acf7765958a1b0e5dc4b13bf76be4426850
qizhe <qizhe.cai@gmail.com>
2021-05-22 14:16:46 -0400

    add change for sender

--- scripts/sender/packet-loss.sh
+++ scripts/sender/packet-loss.sh
@@ -15,23 +15,23 @@ mkdir -p $results_dir
 # No Optimisations
 $DIR/network_setup.py $iface --no-lro --no-gso --no-gro --no-tso --no-arfs --sender --mtu 1500 --sock-size
 for i in 100 1000 10000; do
-    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_no-opts_${i} | $results_dir/packet-loss_no-opts_${i}.log
+    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_no-opts_${i} | tee $results_dir/packet-loss_no-opts_${i}.log
 done
 
 # TSO/GRO
 $DIR/network_setup.py $iface --gro --tso
 for i in 100 1000 10000; do
-    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_tsogro_${i} | $results_dir/packet-loss_tsogro_${i}.log
+    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_tsogro_${i} | tee $results_dir/packet-loss_tsogro_${i}.log
 done
 
 # TSO/GRO+Jumbo Frame
 $DIR/network_setup.py $iface --mtu 9000
 for i in 100 1000 10000; do
-    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_tsogro+jumbo_${i} | $results_dir/packet-loss_tsogro+jumbo_${i}.log
+    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --output $results_dir/packet-loss_tsogro+jumbo_${i} | tee $results_dir/packet-loss_tsogro+jumbo_${i}.log
 done
 
 # TSO/GRO+Jumbo Frame+aRFS
 $DIR/network_setup.py $iface --arfs
 for i in 100 1000 10000; do
-    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --util-breakdown --arfs --output $results_dir/packet-loss_all-opts_${i} | $results_dir/packet-loss_all-opts_${i}.log
+    $DIR/run_experiment_sender.py --receiver $public_dst_ip --addr $device_dst_ip --throughput --utilisation --util-breakdown --arfs --output $results_dir/packet-loss_all-opts_${i} | tee $results_dir/packet-loss_all-opts_${i}.log
 done
